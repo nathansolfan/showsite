6ea77567ed0c8e718576018560ae84e13f61ed8c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-21 14:21:25 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Test Tailwind</title>
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="text-center">
