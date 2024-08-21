1b0a17ab44bffe033e54120882beb4031b8d56a7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-21 13:49:28 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test Tailwind</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
-    <h1>New website</h1>
-
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="text-center">
+        <h1 class="text-4xl font-bold text-gray-800">Tailwind CSS is working!</h1>
+        <p class="text-lg text-gray-600 mt-4">If you see this, Tailwind CSS is successfully integrated.</p>
+    </div>
 </body>
 </html>
