8e1c8a2b6ad4adc949e657b7586120fa56a85a80
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-21 14:27:26 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Test Tailwind</title>
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BqFgHIMm.css') }}">
+<script src="{{ asset('build/assets/app-CUzlGF_f.js') }}" defer></script>
+
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="text-center">
