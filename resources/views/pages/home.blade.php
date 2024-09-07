d8f781d4f4157fa95cf8d46b2874b1d435ab0bea
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-09-07 14:13:29 +0100

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,25 +9,21 @@
                 <span id="typewriter-text" class="text-blue-500"></span>
             </h1>
             <p class="text-xl text-black-200 mt-4">I'm a passionate web developer skilled in both front-end and back-end tasks.</p>
-            <a href="{{ url('/works') }}" class="mt-8 inline-block bg-blue-600 text-white py-3 px-6 rounded-full hover:bg-blue-700">See My Work</a>
+            {{-- <a href="{{ url('/works') }}" class="mt-8 inline-block bg-blue-600 text-white py-3 px-6 rounded-full hover:bg-blue-700">See My Work</a> --}}
         </div>
     </div>
 
     @include('pages.skillsStatic')
 
-    <!-- Dog API Project Section -->
-    <section id="dog-api-project" class="py-20 bg-gray-100">
-        <div class="container mx-auto">
-            <h2 class="text-4xl font-bold text-center text-gray-800">Dog API Project</h2>
-            <div id="dog-api-container" class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-10">
-                <!-- Dog API project will be dynamically loaded here -->
-            </div>
-        </div>
-    </section>
+    <div class="mb-8">
+        @include('sections.dogApi')
+    </div>
+
+
 
     <!-- Add your JavaScript at the end of the body to load after the page content -->
     <script type="module">
-        import { createDogApiProjectCard } from '/js/dogApi.js';
+        // import { createDogApiProjectCard } from '/js/dogApi.js';
 
         document.addEventListener('DOMContentLoaded', function() {
             const texts = ["Web Developer", "Designer", "Freelancer", "Learner"];
@@ -52,12 +48,11 @@
                     setTimeout(type, 150);
                 }
             }
-
             type();
 
             // Load Dog API Project
-            const container = document.getElementById('dog-api-container');
-            createDogApiProjectCard(container);  // Call the function after loading the script
+            // const container = document.getElementById('dog-api-container');
+            // createDogApiProjectCard(container);  // Call the function after loading the script
         });
     </script>
 
