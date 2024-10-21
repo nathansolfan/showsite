987da6bf9537fb0598b0d7d57808e37dee9c7772
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-21 22:38:37 +0100

    updates

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,14 +16,22 @@
     <nav class="w-full py-6 bg-white shadow">
         <div class="container mx-auto flex items-center justify-between">
             <div class="text-lg font-bold">Nathan Ferreira</div>
+
+            <!-- Mobile menu button -->
             <div class="block lg:hidden">
-                <button id="nav-toggle" class="flex items-center px-3 py-2 border rounded text-gray-800 border-gray-400 hover:text-gray-600 hover:border-gray-600">
+                <button
+                id="nav-toggle"
+                aria-label="Toggle Navigation Menu"
+                class="flex items-center px-3 py-2 border rounded text-gray-800 border-gray-400 hover:text-gray-600 hover:border-gray-600">
                     <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <title>Menu</title>
                         <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
                     </svg>
                 </button>
             </div>
+
+
+            <!-- Navigation links -->
             <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
                 <div class="lg:flex-grow">
                     <a href="/" class="block lg:inline-block text-gray-800 hover:text-gray-600 mx-4">Home</a>
