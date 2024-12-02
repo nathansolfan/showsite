38d562c0012badf4804af5e1b25cdf97f7f3f89e
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-02 16:23:47 +0000

    updates

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Feedback;
 use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
-    //
+    public function index()
+    {
+        // fetch feedback to display
+        $feedbacks = Feedback::latest()->take(6)->get();
+        return view('pages.feedback', compact('feedbacks'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|string|max:1000',
+        ]);
+
+        // save
+        Feedback::create($request->all());
+
+        return redirect()->route('feedback')->with('success', 'Thank you for your feedback');
+    }
 }
