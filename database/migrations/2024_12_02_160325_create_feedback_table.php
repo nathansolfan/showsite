93045cd341c8929edbca8b59120b60cb97762b4d
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-12-02 16:14:48 +0000

    updates

--- database/migrations/2024_12_02_160325_create_feedback_table.php
+++ database/migrations/2024_12_02_160325_create_feedback_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('feedback', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->text('message');
             $table->timestamps();
         });
     }
