2756d6927a0637431a0a0a24c31f5ebbdcb84892
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-02-04 22:03:20 +0000

    update

--- resources/views/pages/portfolio2.blade.php
+++ resources/views/pages/portfolio2.blade.php
@@ -11,7 +11,10 @@
         <div class="solar-system">
             <div class="sun"></div>
             <div class="orbit">
-                <div class="planet"></div>
+                <div class="planet-container">
+                    <div class="rocket-icon">🚀</div> <!-- Rocket inside planet, stays upright -->
+                    <div class="planet"></div>
+                </div>
             </div>
         </div>
     </div>
