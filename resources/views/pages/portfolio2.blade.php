4bed94bbd8a9af1ff02b7743a1b838e38b1acd3e
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-02-05 15:07:39 +0000

    updates

--- resources/views/pages/portfolio2.blade.php
+++ resources/views/pages/portfolio2.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Solar System</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-    <div class="solar-system-wrapper">
-        <div class="solar-system">
-            <div class="sun"></div>
-            <div class="orbit">
-                <div class="planet-container">
-                    <div class="rocket-icon">🚀</div> <!-- Rocket inside planet, stays upright -->
-                    <div class="planet"></div>
-                </div>
+  </head>
+  <body>
+    <div class="system-solar">
+      <div class="planets-container">
+        <div class="sun"></div>
+        <div class="planet-index" id="first-planet-index">
+          <div class="route">
+            <div class="planet-container" id="first-planet-container">
+              <div class="planet" id="first-planet"></div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</body>
+  </body>
 </html>
