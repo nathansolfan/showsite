f371be75b337a2ff06f8eefdad63be65059a25f5
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-02-24 20:20:02 +0000

    uopdate

--- resources/views/sections/box2.blade.php
+++ resources/views/sections/box2.blade.php
@@ -28,30 +28,29 @@
     ];
 @endphp
 
-<div class="min-h-screen w-full flex items-center  ">
-<div
-    class="flex flex-col justify-between  w-[90%] max-w-7xl h-auto
- mx-auto bg-yellow-400  border-2 border-gray-900 rounded-lg shadow-lg ">
+<div class="min-h-screen w-full flex items-center">
+  <div class="flex flex-col justify-between w-[90%] max-w-7xl h-auto mx-auto bg-yellow-400 border-2 border-gray-900 rounded-lg shadow-lg">
 
-    {{-- header  border-b-2 --}}
-    <div class="flex justify-between items-center  ">
+    {{-- header --}}
+    <div class="flex justify-between items-center">
         {{-- logo --}}
-        <div class="text-2xl font-extrabold text-gray-900  ">
-            <div class="scale-125">
+        <div class="text-2xl font-extrabold text-gray-900">
+            <div class="text-2xl scale-125 font-extrabold text-gray-900 transition-transform duration-300 transform hover:scale-150">
                 @include('svgs.doglogo')
             </div>
-
         </div>
         {{-- buttons mid --}}
-        <div class="flex flex-wrap justify-center gap-2 -mt-12 pr-2 ">
-            <button class="px-4 text-4xl"> <i class="fa-brands fa-github"></i> </button>
-            <button class="px-4 text-4xl"> <i class="fa-solid fa-eye"></i> </button>
+        <div class="flex flex-wrap justify-center gap-2 -mt-12 pr-2">
+            <button class="px-4 text-4xl transition duration-300 transform hover:scale-110">
+                <i class="fa-brands fa-github"></i>
+            </button>
+            <button class="px-4 text-4xl transition duration-300 transform hover:scale-110">
+                <i class="fa-solid fa-eye"></i>
+            </button>
         </div>
-
         {{-- right --}}
-        <div class="flex flex-wrap sm:flex-row flex-col gap-4  justify-center items-center -mt-12 pr-2">
-
-            <button class="flex items-center gap-2">
+        <div class="flex flex-wrap sm:flex-row flex-col gap-4 justify-center items-center -mt-12 pr-2">
+            <button class="flex items-center gap-2 transition duration-300 transform hover:rotate-12">
                 @include('svgs.nexticon')
             </button>
         </div>
@@ -62,31 +61,29 @@
         {{-- left --}}
         <div class="p-8">
             <div>
-                <h1 class="text-4xl font-bold text-gray-900">🐾 <br> The Ultimate Dog Image Generator! Choose your favorite dog or change to a new one
+                <h1 class="text-4xl font-bold text-gray-900">
+                    🐾 <br> The Ultimate Dog Image Generator! Choose your favorite dog or change to a new one
                 </h1>
-                <p class="text-lg text-gray-700 mt-2">This app generates
-                    random adorable dog images using an API. </p>
+                <p class="text-lg text-gray-700 mt-2">
+                    This app generates random adorable dog images using an API.
+                </p>
             </div>
         </div>
 
-
-
-        {{-- right --}}
-        <div class="flex justify-center mt-4 ">
-            {{-- <img src="{{ asset('images/dogsAI.jpg') }}" alt="Dog API Project" class="w-60 h-auto rounded-lg shadow-md"> --}}
-            <div class="">
+        {{-- center --}}
+        <div class="flex justify-center mt-4">
+            <div class="transition duration-300 transform hover:scale-105">
                 @include('svgs.dogs') <!-- This will inline the code from dog.blade.php -->
             </div>
         </div>
 
-
+        {{-- right --}}
         <div class="flex flex-col justify-center lg:items-end md:items-end items-center p-4 gap-6">
             @foreach ($project['tech'] as $item)
-                <div class="tech-item flex flex-col ">
+                <div class="tech-item flex flex-col transition duration-300 transform hover:scale-110">
                     @if (isset($item['image']))
-                        {{-- <img src="{{ asset($item['image']) }}" class="w-12 h-12 object-contain" alt="{{ $item['title'] }}"> --}}
-                        <div class="w-14 h-14 md:w-14 md:h-14 flex justify-center ">
-                            @include('svgs.expoicon') <!-- This will inline the code from dog.blade.php -->
+                        <div class="w-14 h-14 md:w-14 md:h-14 flex justify-center">
+                            @include('svgs.expoicon')
                         </div>
                     @else
                         <i class="{{ $item['icon'] }} text-5xl text-gray-900"></i>
@@ -95,8 +92,6 @@
             @endforeach
         </div>
 
-
     </div>
+  </div>
 </div>
-</div>
-
