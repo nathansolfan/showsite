d460649b889bf61aa7d1ccd39be57e7fead5f960
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-09 16:32:37 +0000

    created the TrackVisitt middleware and added it on the app.php on bootstrap

--- app/Http/Middleware/TrackVisits.php
+++ app/Http/Middleware/TrackVisits.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Visit;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,25 @@ class TrackVisits
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!$this->isBot($request) && !$this->isAsset($request)) {
+            Visit::create([
+                'ip' => $request->ip(),
+                'user_agent' => $request->userAgent(),
+                'page' => $request->path(),
+            ]);
+        }
         return $next($request);
+    }
+
+    private function isBot($request)
+    {
+        $userAgent = $request->userAgent();
+        return preg_match('/bot|crawl|slurp|spider|mediapartners/i', $userAgent);
+    }
+
+    private function isAsset($request)
+    {
+        $path = $request->path();
+        return preg_match('/bot|crawl|slurp|spider|mediapartners/i', $path);
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(append: [
+            // Existing middleware
+            \App\Http\Middleware\TrackVisits::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
