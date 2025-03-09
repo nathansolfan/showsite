c0a63c2b10c220a051605df4ff16683260e5f297
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-03-09 16:38:23 +0000

    created controller index(_)

---
+++ app/Http/Controllers/VisitController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Visit;
+use Illuminate\Http\Request;
+
+class VisitController extends Controller
+{
+    public function index()
+    {
+        $visits = Visit::latest()->paginate(50);
+        $totalVisits = Visit::count();
+        $uniqueIPs = Visit::distinct('ip')->count('ip');
+        $popularPages = Visit::select('page', \DB::raw('count(*) as total'))
+        ->groupBy('page')
+        ->orderBy('total', 'desc')
+        ->take(10)
+        ->get();
+
+        return view('visits.index', compact('visits', 'totalVisits', 'uniqueIPs', 'popularPages'));
+    }
+}
