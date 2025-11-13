1b46dd76f4c069f7ba520ecda600550d8a38d649
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-11-13 12:35:43 +0000

    up

--- resources/views/api/weatherApi.blade.php
+++ resources/views/api/weatherApi.blade.php
@@ -13,26 +13,29 @@
 </div>
 
 <script>
-    async function loadQuotes() {
-        const response = await fetch('/quotes');
-        const data = await response.json();
+    $.ajax()
 
-        const board = document.getElementById('quote-board');
-        board.innerHTML = '';
 
-        data.slice(0, 5).forEach(q => {
-            const card = document.createElement('div');
-            card.innerHTML = `
-                <p style="font-size: 18px; font-style: italic;">"${q.q}"</p>
-                <p style="text-align: right; font-weight: bold;">— ${q.a}</p>
-            `;
 
-            card.style.marginBottom = '30px';
-            card.style.padding = '20px';
-            card.style.background = '#f9f9f9';
-            card.style.borderLeft = '5px solid #ccc';
-            board.appendChild(card);
-        });
-    }
-    loadQuotes();
 </script>
+
+
+
+
+{{--<script>--}}
+{{--    fetch('/quotes')--}}
+{{--        .then(res => res.json())--}}
+{{--        .then(data => {--}}
+{{--            const board = document.getElementById('quote-board');--}}
+{{--            board.innerHTML = ''; //clean to get new quotes--}}
+
+{{--            data.slice(0, 5).forEach(quote => {--}}
+{{--                board.innerHTML += `--}}
+{{--                    <div style="margin-bottom: 30px; padding: 20px; background: #f9f9f9; border-left: 5px solid #ccc;">--}}
+{{--                        <p style="font-size: 18px; font-style: italic;">"${quote.q}"</p>--}}
+{{--                        <p style="text-align: right; font-weight: bold;">— ${quote.a}</p>--}}
+{{--                    </div>--}}
+{{--                `;--}}
+{{--            });--}}
+{{--        });--}}
+{{--</script>--}}
