f6ffe65b3ead66a256ab24e894aa9b5dfb3207bc
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-11-15 16:10:56 +0000

    up

--- app/Http/Controllers/TextAnalysisController.php
+++ app/Http/Controllers/TextAnalysisController.php
@@ -8,26 +8,55 @@ class TextAnalysisController extends Controller
 {
     public function analysis(Request $request)
     {
-        $text = $request->input('text');
+        $text = strtolower($request->input('text'));
+        //get words
         $words = str_word_count($text, 1);
 
-        //FINN
         $afinn = json_decode(file_get_contents(storage_path('app/afinn/afinn.json.txt')), true);
         $score = 0;
 
         foreach ($words as $word) {
+            $word = trim($word, "\t\n\r\0\x0B.,;:!?'\"()[]{}");
             if (isset($afinn[$word])) {
                 $score += $afinn[$word];
             }
         }
         $feeling = $score > 0 ? 'positive' : ($score < 0 ? 'negative' : 'neutral');
 
+        //NRC file i generated
+        $nrc = json_decode(file_get_contents(storage_path('app/nrc.json')), true);
+
+        $emotions = [
+            'joy' => 0,
+            'anger' => 0,
+            'fear' => 0,
+            'sadness' => 0,
+            'disgust' => 0,
+            'trust' => 0,
+            'anticipation' => 0,
+            'surprise' => 0
+        ];
+
+        //count emotions
+        foreach ($words as $word) {
+            $word = trim(strtolower($word),  " \t\n\r\0\x0B.,;:!?'\"()[]{}");
+            if (isset($nrc[$word])) {
+                foreach ($nrc[$word] as $emotion => $v ) {
+                    if (isset($emotions[$emotion])) {
+                        $emotions[$emotion] += 1;
+                    } else {
+                        $emotions[$emotion] = 1;
+                    }
+                }
+            }
+        }
         return response()->json([
             'word_count'    => count($words),
             'chars'         => mb_strlen($text),
             'feeling_score' => $score,
-            'feeling'       => $feeling
+            'feeling'       => $feeling,
+            'emotions'      => $emotions
         ]);
-
     }
+
 }
