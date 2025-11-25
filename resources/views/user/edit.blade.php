17750acfddc67f5d751103b9a98d2dccb62b46de
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-11-25 14:57:38 +0000

    up

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
     <x-forms.userForm
-        action="/user/{{$user->id}}/edit"
+        action="/user/{{$user->id}}"
         title="Edit your account"
 {{--        paragraph="Edit your account"--}}
         button="Update"
