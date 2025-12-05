2d03db3550239d7751073dbb0fa86b1bacb06a15
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-05 22:55:29 +0000

    new article blade view

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
 
     <x-forms.userForm
+        title="Edit your account"
         action="/user/{{$user->id}}"
         title="Edit your account"
 {{--        paragraph="Edit your account"--}}
