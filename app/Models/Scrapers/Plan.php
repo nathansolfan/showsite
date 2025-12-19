c0ac3b09b65595a86a88158ebd5caba4bc85a767
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-19 23:12:05 +0000

    new article blade view

--- app/Models/Scrapers/Plan.php
+++ app/Models/Scrapers/Plan.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'subscription_id',
+        'name',
+        'billing_period',
+        'features',
+    ];
+
+    protected $casts = [
+        'features' => 'array'
+    ];
+
+    public function subscription()
+    {
+        return $this->belongsTo(Subscription::class);
+    }
+
+    public function prices()
+    {
+        return $this->hasMany(Price::class);
+    }
+
+    public function currentPrice()
+    {
+        return $this->hasOne(Price::class)
+            ->where('valid_from', '<=', now())
+            ->where(function ($q) {
+                $q->whereNull('valid_until')
+                    ->orWhere('valid_from', '>=', now());
+            })
+            ->latest('valid_from');
+
+    }
 }
