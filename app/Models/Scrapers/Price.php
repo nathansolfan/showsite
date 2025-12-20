25bfe52f22d5112d4526924a51a691010c913404
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-20 22:51:51 +0000

    Price model

--- app/Models/Scrapers/Price.php
+++ app/Models/Scrapers/Price.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Price extends Model
 {
-    //
+    protected $fillable = [
+        'plan_id',
+        'amount',
+        'currency',
+        'valid_from',
+        'valid_until'
+    ];
+
+    protected $casts = [
+        'valid_from' => 'datetime',
+        'valid_until' => 'datetime'
+    ];
+
+    public function plan()
+    {
+        return $this->belongsTo(Price::class);
+    }
 }
