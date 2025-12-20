23b5ec52d814b74e846f41cfdc3fdf907019c4d8
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-20 23:37:20 +0000

    Price model

--- app/Models/Scrapers/Category.php
+++ app/Models/Scrapers/Category.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class);
+    }
 }
