39fc15c41a2bd8a31f0f6955e3d766a2e38666b1
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-24 17:52:39 +0000

    Price model

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,12 +34,7 @@ class User extends Authenticatable
         return $this->hasMany(Booking::class);
 
     }
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    
     protected function casts(): array
     {
         return [
