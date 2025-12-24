746a917f5f1e16183f3521a9877d7f248cebb41e
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-24 17:52:05 +0000

    Price model

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,11 +25,7 @@ class User extends Authenticatable
         'is_admin',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+   
     protected $hidden = [
         'password',
         'remember_token',
