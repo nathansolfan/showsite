4f0df167ad1b778f66c5aa27d1384aa3b30491b7
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-12-27 22:22:23 +0000

    Netflix scraper Service - Scraper - SpotifyScraper

--- app/Services/Scrapers/NetflixScraper.php
+++ app/Services/Scrapers/NetflixScraper.php
@@ -39,7 +39,7 @@ class NetflixScraper implements ScraperInterface
 
                     // DUPES
                     if (!isset($foundPlans[$planName])) {
-                        [$planName] = $price;
+                        $foundPlans[$planName] = $price;
                     }
                 }
             });
