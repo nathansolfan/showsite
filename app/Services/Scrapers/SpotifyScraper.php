3ed217d9bff06ade23c26c3994aef14085a18eb4
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-01 23:58:54 +0000

    Generic Scraper

--- app/Services/Scrapers/SpotifyScraper.php
+++ app/Services/Scrapers/SpotifyScraper.php
@@ -3,8 +3,6 @@
 namespace App\Services\Scrapers;
 
 use App\Contracts\ScraperInterface;
-use Illuminate\Support\Facades\Http;
-use Symfony\Component\DomCrawler\Crawler;
 
 class SpotifyScraper implements ScraperInterface
 {
@@ -12,26 +10,7 @@ class SpotifyScraper implements ScraperInterface
 
     public function scrape(): array
     {
-        $response = Http::timeout(30)
-            ->withoutVerifying()
-            ->withHeaders([
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-            ])
-            ->get($this->url);
+        
 
-        if (!$response->successful()) {
-            throw new \Exception('Failed to fetch Spotify');
-        }
-
-        $crawler = new Crawler($response->body());
-        $plans = [];
-
-        $plans[] = [
-            'name' => 'Indivual',
-            'price' => '11'
-        ];
-
-        return $plans;
     }
-
 }
