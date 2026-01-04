810ba25d97e6e107342f277fd5477411c6a589de
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-04 23:47:52 +0000

    Generic Scraper

---
+++ app/Services/Scrapers/SpotifyScraper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Scrapers;
+
+use App\Contracts\ScraperInterface;
+
+class SpotifyScraper implements ScraperInterface
+{
+    private string $url = 'https://www.spotify.com/uk/premium/';
+
+    public function scrape(): array
+    {
+
+
+    }
+}
