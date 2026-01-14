691a7786e4819ec06a2c55f40a2958b2cb0c8af3
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-14 23:54:32 +0000

    Generic Scraper

--- app/Services/Scrapers/DisneyScraper.php
+++ app/Services/Scrapers/DisneyScraper.php
@@ -8,19 +8,21 @@ class DisneyScraper implements ScraperInterface
 {
     private string $url = 'https://www.disneyplus.com/en-gb/welcome/plans';
 
-    public function scrape()
+    public function scrape(): array
     {
         try {
             //1st try
             $plans = $this->scrapeReal();
 
+            //try if plans ok
             if (!empty($plans) && $this->plansLookValid($plans)) {
                 return $plans;
             }
         } catch (\Exception $exception) {
             \Log::warning('Disney failed: ' . $exception->getMessage());
         }
-
+        //if fail get fallback
+        return $this->getFallbackPlans();
     }
 
 }
