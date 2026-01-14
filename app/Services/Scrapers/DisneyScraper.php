dca2624d0ab324ed6f7ffcd99067d626e01e3dd1
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-14 23:52:09 +0000

    Generic Scraper

--- app/Services/Scrapers/DisneyScraper.php
+++ app/Services/Scrapers/DisneyScraper.php
@@ -6,4 +6,17 @@ class DisneyScraper
 {
     private string $url = 'https://www.disneyplus.com/en-gb/welcome/plans';
 
+    public function scrape()
+    {
+        try {
+            //1st try
+            $plans = $this->scrapeReal();
+
+            if (!empty($plans) && $this->plansLookValid($plans)) {
+                return $plans;
+            }
+        }
+
+    }
+
 }
