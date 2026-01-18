5eaf876588fa713279d743fdbc4e1ab7c1471bf4
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-18 12:11:47 +0000

    Generic Scraper

--- app/Services/LoadUp/ReferralService.php
+++ app/Services/LoadUp/ReferralService.php
@@ -16,7 +16,41 @@ class ReferralService
 //            return $this->noReferral();
 //        }
 
+        $okCode = strtoupper(trim($referralCode));
 
+        $referrer = User::where('users.referral_code', $okCode)->first();
+
+        if ($referrer = null) {
+            return $this->noReferral();
+        }
+
+        if ($currentUser !== null && $referrer->id === $currentUser->id) {
+            return $this->noReferral();
+        }
+
+        return [
+            'discount_amount' => 05.00,
+            'referred_by_user_id' => $referrer->id,
+            'referral_code_used' => $okCode,
+        ];
     }
 
+    private function noReferral(): array
+    {
+        return [
+            'discount_amount' => 0,
+            'referred_by_user_id' => null,
+            'referral_code_used' => null
+        ];
+    }
 }
+
+
+
+
+
+
+
+
+
+
