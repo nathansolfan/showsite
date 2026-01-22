0a2e9f7e99a65b4576a161b7cc8a104d62a9f734
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-22 21:57:39 +0000

    LoadUp updates

---
+++ app/Http/Controllers/SubscriptionsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Scrapers\Category;
+use App\Models\Scrapers\Subscription;
+
+class SubscriptionsController extends Controller
+{
+    public function index()
+    {
+        $subscriptions = Subscription::with('category')
+            ->orderBy('price', 'asc')
+            ->get()
+            ->groupBy('category.name');
+
+        $categories = Category::withCount('subscriptions')->get();
+
+        return view('subscriptions.index', [
+            'subscriptions' => $subscriptions,
+            'categories' => $categories
+        ]);
+    }
+}
