c5e5b0b533b78ced663ad8290bab44c9b69f204d
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-22 21:29:54 +0000

    LoadUp updates

--- database/migrations/2025_12_19_212604_create_subscriptions_table.php
+++ database/migrations/2025_12_19_212604_create_subscriptions_table.php
@@ -10,15 +10,19 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('subscriptions', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->string('website_url')->nullable();
-            $table->string('logo')->nullable();
-            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
-            $table->timestamps();
-        });
+        if (Schema::hasTable('subscriptions')) {
+            Schema::create('subscriptions', function (Blueprint $table) {
+
+                $table->id();
+                $table->string('name');
+                $table->string('slug')->unique();
+                $table->string('website_url')->nullable();
+                $table->string('logo')->nullable();
+                $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+                $table->timestamps();
+            });
+        }
+
     }
 
     /**
