5b856cd1d5b80c0cef5d948c2f6511fac785e9bd
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-01-27 23:24:47 +0000

    LoadUp updates

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -3,10 +3,9 @@
     <x-slot name="title">Compare UK Subscriptions Prices and Save Money</x-slot>
 
 
+    {{--hero--}}
     <section>
-
         <div>
-
             <h1>Compare Subscriptions Prices</h1>
 
             <p>Find the best deals on streaming, music and more</p>
@@ -17,8 +16,18 @@
                 <a href="/subscriptions/compare">Compare Now</a>
             </div>
         </div>
-
-
     </section>
 
+    {{--stats--}}
+    <section>
+        <div>
+            <div>
+                <div>
+                    {{ $subscriptions->flatten()->count() }}
+                </div>
+                <div>Services Compared</div>
+            </div>
+        </div>
+
+    </section>
 </x-layout>
