cafac04a8445b9cd76d9eef50a8fd57ceb990333
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-02-03 21:03:03 +0000

    LoadUp updates

--- database/migrations/2026_02_03_210232_add_affiliate_url_to_subscriptions_table.php
+++ database/migrations/2026_02_03_210232_add_affiliate_url_to_subscriptions_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('subscriptions', function (Blueprint $table) {
-            //
+            $table->string('affiliate_url')->nullable()->after('website_url');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('subscriptions', function (Blueprint $table) {
-            //
+            $table->dropColumn('affiliate_url');
         });
     }
 };
