b27a8ea90184ee175984d3c91b427c87488552c4
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2026-02-11 22:08:46 +0000

    LoadUp updates

--- resources/views/subscriptions/show.blade.php
+++ resources/views/subscriptions/show.blade.php
@@ -106,16 +106,16 @@
                 @endif
 
                 <!-- Back Button -->
-                <div class="mt-12 text-center">
-                    <a href="{{ route('subscriptions.index') }}"
-                       class="inline-flex items-center text-purple-600 hover:text-purple-700 font-semibold">
-                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M15 19l-7-7 7-7"></path>
-                        </svg>
-                        Back to All Services
-                    </a>
-                </div>
+                {{--                <div class="mt-12 text-center">--}}
+                {{--                    <a href="{{ route('subscriptions.index') }}"--}}
+                {{--                       class="inline-flex items-center text-purple-600 hover:text-purple-700 font-semibold">--}}
+                {{--                        <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">--}}
+                {{--                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+                {{--                                  d="M15 19l-7-7 7-7"></path>--}}
+                {{--                        </svg>--}}
+                {{--                        Back to All Services--}}
+                {{--                    </a>--}}
+                {{--                </div>--}}
             </div>
         </div>
     </section>
